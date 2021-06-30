245d297cc13f680cfa1fd611c866d11e4966fcf8
0rakul0 <0rakul0render@gmail.com>
2021-06-30 08:07:58 -0300

    sala e questões

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import { FormEvent, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 
@@ -5,6 +6,7 @@ import logoImg from '../assets/images/logo.svg'
 
 import { Button } from '../components/Button'
 import { CodeRoom } from '../components/CodeRoom'
+import { Question } from '../components/Questions'
 import { useAuth } from '../hooks/useAuth'
 import { database } from '../services/firebase'
 
@@ -20,7 +22,7 @@ type FirebaseQuestions = Record<string, {
   isHighlighted: boolean;
 }>
 
-type Question = {
+type QuestionType = {
   id: string;
   author: {
     name: string;
@@ -40,7 +42,7 @@ export function Room() {
   const { user } = useAuth();
   const params = useParams<RoomParams>();
   const [newQuestion, setNewQuestion] = useState('');
-  const [questions, setQuestions] = useState<Question[]>([])
+  const [questions, setQuestions] = useState<QuestionType[]>([])
   const [title, setTitle] = useState('');
 
   const roomId = params.id;
@@ -127,7 +129,17 @@ export function Room() {
           </div>
         </form>
 
-        {JSON.stringify(questions)}
+        <div className="question-list">
+        {questions.map(question => {
+          return(
+            <Question 
+              key={question.id}
+              content ={question.content}
+              author = {question.author}
+            />
+          )
+        })}
+        </div>
       </main>
     </div>
   );
